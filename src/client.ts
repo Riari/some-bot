ea280075faf250d55c7620c07071ed69f87f352a
Riari <riari@teamteatime.net>
2018-10-08 21:00:05 +0100

    Fix entry script path and client import

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
-import Discord from 'discord.js'
+import { Client } from 'discord.js'
 
-const client = new Discord.Client()
+const client = new Client
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
